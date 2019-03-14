d0ec93e73e60ea7bc67c68cf2eb9a8678e11ad60
Alex4386 <psh010209@gmail.com>
2019-03-14 15:29:46 +0900

    add: I guess this is the end of 02

---
+++ 02/02/commentOut.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	/*
+		----------------------------------
+		The following are special symbols.
+		!@#$%^&*()_+\
+
+		This is the end of my C program.
+
+		----------------------------------
+	*/
+}
---
+++ 02/02/specialCharacterOutput.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+	printf("\t----------------------------------\n");
+	printf("\tThe following are special symbols.\n");
+	printf("\t!@#$%^&*()_ +\\\n");
+	printf("\n");
+	printf("\tThis is the end of my C program.\n");
+	printf("\n");
+	printf("\t----------------------------------");
+}
